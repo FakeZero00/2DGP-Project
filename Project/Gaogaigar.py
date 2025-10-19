7927809597a410b5cc7d6311c9bb80e989af3899
FakeZero <sjlimdaa@naver.com>
2025-10-20 02:30:12 +0900

    Run 상태 코드 추가

--- Project/Gaogaigar.py
+++ Project/Gaogaigar.py
@@ -10,12 +10,13 @@ class StateMachine:
     def draw(self):
         self.cur_state.draw()
 
+# 상태 클래스들
 class Idle:
     def __init__(self, gaogaigar):
         self.gaogaigar = gaogaigar
 
     def enter(self):
-        pass
+        self.gaogaigar.frame = 0
 
     def exit(self):
         pass
@@ -26,6 +27,22 @@ class Idle:
     def draw(self):
         self.gaogaigar.image.clip_draw(400, 400, 400, 400, self.gaogaigar.x, self.gaogaigar.y)
 
+class Run:
+    def __init__(self, gaogaigar):
+        self.gaogaigar = gaogaigar
+
+    def enter(self):
+        self.gaogaigar.frame = 0
+
+    def exit(self):
+        pass
+
+    def do(self):
+        self.gaogaigar.frame = (self.gaogaigar.frame + 1) % 10
+
+    def draw(self):
+        self.gaogaigar.image.clip_draw(self.gaogaigar.frame * 400, 0, 400, 400, self.gaogaigar.x, self.gaogaigar.y)
+
 class Gaogaigar:
     def __init__(self):
         self.image = load_image('../Sprite/Move_Sprite(temp_resize).png')
@@ -33,10 +50,12 @@ class Gaogaigar:
         self.frame = 0
 
         self.IDLE = Idle(self)
-        self.statemachine = StateMachine(self.IDLE)
+        self.RUN = Run(self)
+        self.statemachine = StateMachine(self.RUN)
 
     def update(self):
-        pass
+        self.statemachine.update()
 
     def draw(self):
-        self.image.clip_draw(0, 400, 400, 400, self.x, self.y)
+        self.statemachine.draw()
+
