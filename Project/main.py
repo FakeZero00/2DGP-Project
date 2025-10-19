488dc5f4889a0fb738c50eaa9ced1550345a60e2
FakeZero <sjlimdaa@naver.com>
2025-10-20 01:28:54 +0900

    가오가이거 그리는 함수, 오브젝트 관리를 위한 월드 함수 추가

--- Project/main.py
+++ Project/main.py
@@ -2,18 +2,37 @@ from pico2d import *
 
 open_canvas(1600, 900)
 
-
 class Gaogaigar:
     def __init__(self):
-        self.image = load_image('Sprite/Move_Sprite(temp_resize).png')
+        self.image = load_image('../Sprite/Move_Sprite(temp_resize).png')
         self.x, self.y = 800, 450
         self.frame = 0
 
-        def update(self):
-            pass
+    def update(self):
+        pass
 
-        def draw(self):
-            self.image.clip_draw(self.frame * 400, 0, 400, 400, self.x, self.y)
+    def draw(self):
+        self.image.clip_draw(0, 400, 400, 400, self.x, self.y)
+
+#========= 월드 함수 ==========
+def reset_world():
+    global world
+    global gaogaigar
+
+    world = []
+
+    gaogaigar = Gaogaigar()
+    world.append(gaogaigar)
+
+def update_world():
+    for o in world:
+        o.update()
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 #Input 이벤트 처리 함수
 def Input_Event():
@@ -28,8 +47,12 @@ def Input_Event():
 Window_Running = True
 
 #========= 메인 루프 ==========
+reset_world()
+
 while Window_Running:
     Input_Event()
-
+    update_world()
+    render_world()
+    delay(1 / 10)
 
 close_canvas()
