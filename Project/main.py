541be43dd9f1b5d3f4df879a8d592e8f5c53f001
FakeZero <sjlimdaa@naver.com>
2025-10-20 00:37:26 +0900

    Input 이벤트 처리 함수 추가, 창닫기 버튼으로 창 닫기 코드 추가

--- Project/main.py
+++ Project/main.py
@@ -2,7 +2,23 @@ from pico2d import *
 
 open_canvas(1600, 900)
 
-while True:
-    pass
+
+
+
+#Input 이벤트 처리 함수
+def Input_Event():
+    global Window_Running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT: # 창 닫기 버튼
+            Window_Running = False
+
+#========= 전역 변수 ==========
+Window_Running = True
+
+#========= 메인 루프 ==========
+while Window_Running:
+    Input_Event()
 
 close_canvas()
