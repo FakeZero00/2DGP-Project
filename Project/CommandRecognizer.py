9972d5e4c6054689131080209ae897b24d7a1f9c
FakeZero <sjlimdaa@naver.com>
2025-11-08 01:28:52 +0900

    Command Buffer 분리

--- Project/CommandRecognizer.py
+++ Project/CommandRecognizer.py
@@ -1,3 +1,33 @@
+from pico2d import get_time
+from typing import List, Tuple, Optional
+
+#커맨드 버퍼 클래스, 인스턴스 생성
+class CommandBuffer:
+    def __init__(self, BoundTime: float = 1.0):
+        self.BoundTime = BoundTime
+        self.buffer: List[Tuple[str, float]] = []
+
+    def add(self, token: str, t: Optional[float] = None):
+        if t is None:
+            t = get_time()
+        self.buffer.append((token, t))
+        self.pop_old(t)
+
+    def pop_old(self, now: float):
+        cutoff = now - self.BoundTime
+        self.buffer = [(tok, ts) for tok, ts in self.buffer if ts >= cutoff]
+
+    def tokens(self) -> List[str]:
+        return [tok for tok, _ in self.buffer]
+
+    def last_token(self):
+        return self.buffer[-1][0] if self.buffer else None
+
+    def clear_last_n(self, n):
+        for _ in range(min(n, len(self.buffer))):
+            self.buffer.pop()
+
+#  커맨드 인식기 클래스
 class CommandRecognizer:
     def __init__(self, patterns):
         # 미리 입력해둔 패턴들을 긴 패턴을 먼저 감지하기 위해 길이의 내림차순으로 정렬
--- Project/Gaogaigar.py
+++ Project/Gaogaigar.py
@@ -1,34 +1,9 @@
 from pico2d import load_image, get_time
 from sdl2 import *
-from typing import List, Tuple, Optional
-from Project.CommandRecognizer import CommandRecognizer
+from Project.CommandRecognizer import CommandBuffer, CommandRecognizer
 from Project.State_Machine import StateMachine
 
-#커맨드 버퍼 클래스, 인스턴스 생성
-class CommandBuffer:
-    def __init__(self, BoundTime: float = 1.0):
-        self.BoundTime = BoundTime
-        self.buffer: List[Tuple[str, float]] = []
 
-    def add(self, token: str, t: Optional[float] = None):
-        if t is None:
-            t = get_time()
-        self.buffer.append((token, t))
-        self.pop_old(t)
-
-    def pop_old(self, now: float):
-        cutoff = now - self.BoundTime
-        self.buffer = [(tok, ts) for tok, ts in self.buffer if ts >= cutoff]
-
-    def tokens(self) -> List[str]:
-        return [tok for tok, _ in self.buffer]
-
-    def last_token(self):
-        return self.buffer[-1][0] if self.buffer else None
-
-    def clear_last_n(self, n):
-        for _ in range(min(n, len(self.buffer))):
-            self.buffer.pop()
 
 command_buffer = CommandBuffer()
 
