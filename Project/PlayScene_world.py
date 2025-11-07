030f16651af15d5cb22be866aea9ce878df20a06
FakeZero <sjlimdaa@naver.com>
2025-11-08 00:37:01 +0900

    레이어 기능 추가

--- Project/PlayScene_world.py
+++ Project/PlayScene_world.py
@@ -1,12 +1,25 @@
-world = []
+# 0번 레이어: 배경 레이어, 1번 레이어: 기본 레이어
+world = [[], []]
 
-def add_object(o):
-    world.append(o)
+def add_object(o, depth = 1):
+    world[depth].append(o)
+
+def add_objects(ol, depth = 1):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('Cannot find object in world')
 
 def update():
-    for o in world:
-        o.update()
+    for layer in world:
+        for o in layer:
+            o.update()
 
 def render():
-    for o in world:
-        o.draw()
+    for layer in world:
+        for o in layer:
+            o.draw()
