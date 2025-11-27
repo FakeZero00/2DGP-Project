74ba7bcaa73fa320e2f21b8f68ec7787b57a334b
FakeZero <sjlimdaa@naver.com>
2025-11-28 03:09:41 +0900

    건담 클래스 추가

---
+++ Project/Gundam.py
@@ -0,0 +1,396 @@
+from pico2d import load_image, draw_rectangle
+from Project.CommandRecognizer import CommandBuffer, CommandRecognizer
+from Project.State_Machine import StateMachine
+from Project.Event_Function import *
+import Game_Framework
+
+#커맨드 버퍼
+command_buffer = CommandBuffer()
+
+#커맨드 목록
+CommandList = [
+    (('DOWN', 'RIGHTDOWN', 'RIGHT', 'ATTACK'), 'COMMAND_SKILL'),
+    (('DOWN', 'RIGHTDOWN', 'RIGHT', 'IDLE', 'ATTACK'), 'COMMAND_SKILL')
+]
+Recognizer = CommandRecognizer(CommandList)
+
+#커맨드, INPUT 등 이벤트 함수
+input_booleans = {input_key : False for input_key in ['w', 'a', 's', 'd']}
+def cmd_is(name):
+    if name == 'COMMAND_SKILL':
+        return cmdskill_start
+
+def cmdskill_start(e, object_state):
+    return e[0] == 'CMD' and e[1] == 'COMMAND_SKILL'
+
+#기준 프레임
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+
+#이동 속도
+RUN_SPEED_PPS = 1000
+DROP_SPEED_PPS = 100
+ATTACK_MOVE_SPEED_PPS = 200
+
+# 상태 클래스들
+class Idle:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 4
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        pass
+
+    def draw(self):
+        if self.gundam.jump_bool:
+            self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+        else:
+            self.gundam.image.clip_draw(0, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Run:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        if not self.gundam.jump_bool:
+            self.gundam.frame = (self.gundam.frame + 10 * ACTION_PER_TIME * Game_Framework.frame_time) % 10
+
+        self.gundam.x += RUN_SPEED_PPS * Game_Framework.frame_time
+        if self.gundam.x + 225 > 1600:
+            self.gundam.x = 1600 - 225
+
+    def draw(self):
+        if self.gundam.jump_bool:
+            self.gundam.image.clip_draw(2 * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+        else:
+            self.gundam.image.clip_draw(int(self.gundam.frame) * 800, 800, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Back:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 1
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        self.gundam.x -= RUN_SPEED_PPS * Game_Framework.frame_time
+        if(self.gundam.x - 225 < 0):
+            self.gundam.x = 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Crouch:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 3
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        pass
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Crouch_Rightdown:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 3
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        pass
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Crouch_Leftdown:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 3
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        pass
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Jump:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 4
+        if not self.gundam.jump_bool:
+            self.gundam.jump_bool = True
+            self.gundam.jump_frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        pass
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Jump_Leftup:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 1
+        if not self.gundam.jump_bool:
+            self.gundam.jump_bool = True
+            self.gundam.jump_frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        self.gundam.x -= RUN_SPEED_PPS * Game_Framework.frame_time
+        if (self.gundam.x - 225 < 0):
+            self.gundam.x = 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Jump_Rightup:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 2
+        if not self.gundam.jump_bool:
+            self.gundam.jump_bool = True
+            self.gundam.jump_frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        self.gundam.x += RUN_SPEED_PPS * Game_Framework.frame_time
+        if self.gundam.x + 225 > 1600:
+            self.gundam.x = 1600 - 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(self.gundam.frame * 800, 0, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Attack1:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 0
+        self.gundam.cooltime_bool = False
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        input_check(e, input_booleans)
+
+        self.gundam.frame = self.gundam.frame + 10 * ACTION_PER_TIME * Game_Framework.frame_time
+        if int(self.gundam.frame) > 7:
+            self.gundam.statemachine.handle_state_event(('ANIM_END', 0), self.gundam.object_state)
+        elif int(self.gundam.frame) >= 4:
+            self.gundam.cooltime_bool = True
+
+        if self.gundam.y <= 250:
+            self.gundam.x += ATTACK_MOVE_SPEED_PPS * Game_Framework.frame_time
+        if self.gundam.x + 225 > 1600:
+            self.gundam.x = 1600 - 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(min(int(self.gundam.frame), 5) * 800, 800 * 2, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Attack2:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 0
+        self.gundam.cooltime_bool = False
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        input_check(e, input_booleans)
+
+        self.gundam.frame = self.gundam.frame + 10 * ACTION_PER_TIME * Game_Framework.frame_time
+        if int(self.gundam.frame) > 6:
+            self.gundam.statemachine.handle_state_event(('ANIM_END', 0), self.gundam.object_state)
+        elif int(self.gundam.frame) >= 3:
+            self.gundam.cooltime_bool = True
+
+        if self.gundam.y <= 250:
+            self.gundam.x += ATTACK_MOVE_SPEED_PPS * Game_Framework.frame_time
+        if self.gundam.x + 225 > 1600:
+            self.gundam.x = 1600 - 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(min(int(self.gundam.frame), 4) * 800, 800 * 3, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Attack3:
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        input_check(e, input_booleans)
+
+        self.gundam.frame = self.gundam.frame + 10 * ACTION_PER_TIME * Game_Framework.frame_time
+        if int(self.gundam.frame) > 8:
+            self.gundam.statemachine.handle_state_event(('ANIM_END', 0), self.gundam.object_state)
+
+        if self.gundam.y <= 250:
+            self.gundam.x += ATTACK_MOVE_SPEED_PPS * Game_Framework.frame_time
+        if self.gundam.x + 225 > 1600:
+            self.gundam.x = 1600 - 225
+
+    def draw(self):
+        self.gundam.image.clip_draw(min(int(self.gundam.frame), 5) * 800, 800 * 4, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+class Command_skill: # Test
+    def __init__(self, gundam):
+        self.gundam = gundam
+
+    def enter(self, e):
+        self.gundam.frame = 0
+
+    def exit(self, e):
+        pass
+
+    def do(self, e):
+        input_check(e, input_booleans)
+        self.gundam.frame = self.gundam.frame + 10 * ACTION_PER_TIME * Game_Framework.frame_time
+        if int(self.gundam.frame) > 100:
+            self.gundam.statemachine.handle_state_event(('ANIM_END', 0), self.gundam.object_state)
+
+    def draw(self):
+        self.gundam.image.clip_draw(min(int(self.gundam.frame), 5) * 800, 800 * 2, 800, 800, self.gundam.x, self.gundam.y, 450, 450)
+
+# 가오가이거 클래스 본체
+class Gundam:
+    def __init__(self):
+        self.image = load_image('../Sprite/Gundam_Sprite.png')
+        self.x, self.y = 300, 250
+        self.yv = 0
+        self.frame = 0
+        self.cur_input_event = None
+        self.cooltime_bool = True
+        self.jump_bool = False
+        self.jump_frame = 0
+        #점프 높이 테이블 (프레임별 y 오프셋)
+        self.jump_table = [0, 20, 50, 90, 140, 200, 270, 300, 330, 350, 380, 400, 400, 400, 400, 400, 400, 380, 350, 330, 300, 270, 200, 140, 90, 50, 20, 0]
+
+        #객체 상태 초기화
+        self.object_state = (command_buffer, input_booleans, self.cooltime_bool, self.jump_bool)
+
+        self.IDLE = Idle(self)
+        self.RUN = Run(self)
+        self.BACK = Back(self)
+        self.CROUCH = Crouch(self)
+        self.CROUCH_RIGHTDOWN = Crouch_Rightdown(self)
+        self.CROUCH_LEFTDOWN = Crouch_Leftdown(self)
+        self.JUMP = Jump(self)
+        self.JUMP_LEFTUP = Jump_Leftup(self)
+        self.JUMP_RIGHTUP = Jump_Rightup(self)
+        self.ATTACK1 = Attack1(self)
+        self.ATTACK2 = Attack2(self)
+        self.ATTACK3 = Attack3(self)
+        self.COMMAND_SKILL = Command_skill(self)
+
+        self.rules = {
+            self.IDLE: {right_down: self.RUN, left_down: self.BACK, right_up: self.BACK, left_up: self.RUN, down_down: self.CROUCH, up_down: self.JUMP, up_up: self.IDLE,
+                        cmd_is('COMMAND_SKILL'): self.COMMAND_SKILL, attack_down: self.ATTACK1},
+            self.RUN: {right_up: self.IDLE, left_down: self.IDLE, right_down: self.IDLE, down_down: self.CROUCH_RIGHTDOWN, up_down: self.JUMP_RIGHTUP,
+                       attack_down: self.ATTACK1},
+            self.BACK: {left_up: self.IDLE, right_down: self.IDLE, down_down: self.CROUCH_LEFTDOWN, up_down: self.JUMP_LEFTUP},
+            self.CROUCH: {down_up: self.IDLE, right_down: self.CROUCH_RIGHTDOWN, left_down: self.CROUCH_LEFTDOWN},
+            self.CROUCH_RIGHTDOWN: {down_up: self.RUN, right_up: self.CROUCH},
+            self.CROUCH_LEFTDOWN: {down_up: self.BACK, left_up: self.CROUCH},
+            self.JUMP: {up_up: self.IDLE, left_down: self.JUMP_LEFTUP, right_down: self.JUMP_RIGHTUP},
+            self.JUMP_LEFTUP: {up_up: self.BACK, left_up: self.IDLE},
+            self.JUMP_RIGHTUP: {up_up: self.RUN, right_up: self.IDLE},
+            self.ATTACK1: {anim_end('BACK'): self.BACK, anim_end('RUN'): self.RUN, anim_end('CROUCH'): self.CROUCH, anim_end('IDLE'): self.IDLE,
+                           attack_down: self.ATTACK2, cmd_is('COMMAND_SKILL'): self.COMMAND_SKILL},
+            self.ATTACK2: {anim_end('BACK'): self.BACK, anim_end('RUN'): self.RUN, anim_end('CROUCH'): self.CROUCH, anim_end('IDLE'): self.IDLE,
+                           attack_down: self.ATTACK3},
+            self.ATTACK3: {anim_end('BACK'): self.BACK, anim_end('RUN'): self.RUN, anim_end('CROUCH'): self.CROUCH, anim_end('IDLE'): self.IDLE},
+            self.COMMAND_SKILL: {anim_end('BACK'): self.BACK, anim_end('RUN'): self.RUN, anim_end('CROUCH'): self.CROUCH, anim_end('IDLE'): self.IDLE}
+        }
+        self.statemachine = StateMachine(self.IDLE, self.rules)
+
+    def update(self):
+        #객체 상태 업데이트
+        self.object_state = (command_buffer, input_booleans, self.cooltime_bool, self.jump_bool)
+
+        #점프 프레임 테이블 적용
+        if self.jump_bool:
+            self.jump_frame += 20 * ACTION_PER_TIME * Game_Framework.frame_time
+            if int(self.jump_frame) < len(self.jump_table):
+                self.y = 250 + self.jump_table[int(self.jump_frame)]
+            else:
+                self.jump_bool = False
+                self.y = 250
+
+        # 현재 입력 이벤트로 상태 머신 업데이트
+        self.statemachine.update(('INPUT', self.cur_input_event))
+        cmd_list = Recognizer.match(command_buffer)
+        if cmd_list:
+            action, used = cmd_list
+            command_buffer.clear_last_n(used) # 매칭된 커맨드만큼 버퍼에서 제거
+            print(f'커맨드 인식: {action}')
+            self.statemachine.handle_state_event(('CMD', action), self.object_state)
+
+
+    def draw(self):
+        self.statemachine.draw()
+        draw_rectangle(*self.get_bb())
+
+    def handle_event(self, event):
+        self.statemachine.handle_state_event(('INPUT', event), self.object_state)
+        self.cur_input_event = event
+
+    def get_bb(self):
+        return self.x - 225, self.y - 225, self.x + 225, self.y + 225
+
+    def handle_collision(self, group, other):
+        # if group == 'gaogaigar:ground':
+        #     self.y = 250
+        #     self.jump_bool = False
+        pass
